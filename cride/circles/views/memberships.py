2a1af1db26be4f0149f931257c7d04268974ef49
MkAngelo <mikequi000@gmail.com>
2021-09-07 10:25:34 -0500

    Member's invitation'

--- cride/circles/views/memberships.py
+++ cride/circles/views/memberships.py
@@ -1,11 +1,13 @@
 """Circle membership view."""
 
 # Django REST Framework
-from rest_framework import mixins, permissions, viewsets
+from rest_framework import mixins, viewsets
 from rest_framework.generics import get_object_or_404
+from rest_framework.decorators import action
+from rest_framework.response import Response
 
 # Models
-from cride.circles.models import Circle, Membership
+from cride.circles.models import Circle, Membership, Invitation, invitations
 
 # Permisions
 from rest_framework.permissions import IsAuthenticated
@@ -54,3 +56,40 @@ class MembershipViewSet(mixins.ListModelMixin,
         """Disable membership."""
         instance.is_active = False
         instance.save()
+
+    @action(detail=True, methods=['get'])
+    def invitations(self, request, *args, **kwargs):
+        """Retrieve a member's invitations breakdown.
+        
+        Will return a list containing all the members that have
+        used its invitations an another list containing the
+        invitations that haven't being used yet.
+        """
+        member = self.get_object()
+        invited_members = Membership.objects.filter(
+            circle=self.circle,
+            invited_by=request.user,
+            is_active=True
+        )
+
+        unused_invitations = Invitation.objects.filter(
+            circle=self.circle,
+            issued_by=request.user,
+            used=False
+        ).values_list('code')
+        diff = member.remaining_invitations - len(unused_invitations)
+
+        invitations = [x(0) for x in unused_invitations]
+        for i in range(0, diff):
+            invitations.append(
+                Invitation.objects.create(
+                    issued_by=request.user,
+                    circle = self.circle
+                ).code
+            )
+
+        data = {
+            'used_invitations': MembershipModelSerializer(invited_members, many=True).data,
+            'invitations': invitations
+        }
+        return Response(data)
