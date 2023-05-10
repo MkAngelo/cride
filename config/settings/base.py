34f6109edc2e26d9a746156fe5b5ed5bb406758e
MkAngelo <mikequi000@gmail.com>
2023-05-10 17:03:21 -0600

    local env solv

--- config/settings/base.py
+++ config/settings/base.py
@@ -87,7 +87,12 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ORIGIN_WHITELIST = (
+    'http://localhost',
+)
 
 # Static files
 STATIC_ROOT = str(ROOT_DIR('staticfiles'))
--- config/settings/local.py
+++ config/settings/local.py
@@ -9,9 +9,7 @@ DEBUG = True
 # Security
 SECRET_KEY = env('DJANGO_SECRET_KEY', default='PB3aGvTmCkzaLGRAxDc3aMayKTPTDd5usT8gw4pCmKOk5AlJjh12pTrnNgQyOHCH')
 ALLOWED_HOSTS = [
-    "localhost",
-    "0.0.0.0",
-    "127.0.0.1",
+    "*",
 ]
 
 # Cache
@@ -31,8 +29,9 @@ EMAIL_HOST = 'localhost'
 EMAIL_PORT = 1025
 
 # django-extensions
-INSTALLED_APPS += ['django_extensions']  # noqa F405
+INSTALLED_APPS += ['corsheaders', 'django_extensions']  # noqa F405
 
 # Celery
 CELERY_TASK_ALWAYS_EAGER = True
 CELERY_TASK_EAGER_PROPAGATES = True
+CORS_ORIGIN_ALLOW_ALL = True
